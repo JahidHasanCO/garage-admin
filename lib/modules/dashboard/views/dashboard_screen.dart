bd6235f4aedf1b5d271e75dfbd6b232e331ff045
Md. Zahidul Islam <vdjsovaj@gmail.com>
2025-09-22 10:11:36 +0600

    feat: Update dashboard screen layout with background color and title

--- lib/modules/dashboard/views/dashboard_screen.dart
+++ lib/modules/dashboard/views/dashboard_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:garage_admin/theme/app_colors.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Expanded(
+      child: Container(
+        color: AppColors.backgroundColor,
+        alignment: Alignment.center,
+        child: const Text(
+          'Dashboard Screen',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
